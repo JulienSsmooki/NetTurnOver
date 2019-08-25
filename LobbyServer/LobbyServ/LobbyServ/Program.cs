bb912a0ccb620a3bbe093a384978e73cea0cb4d5
JulienL <jlcontact.jv@gmail.com>
2019-08-25 17:31:44 +0200

    Test socket
    start server for lobby and a test client to test the connection.

---
+++ LobbyServer/LobbyServ/LobbyServ/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LobbyServ
+{
+    class Program
+    {
+        private static Socket _serverSocket = new Socket(
+                                                AddressFamily.InterNetwork,
+                                                SocketType.Stream,
+                                                ProtocolType.Tcp);
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static byte[] _buffer = new byte[1024];
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            SetupServer(Convert.ToInt32(args[0]));
+            Console.ReadLine();
+        }
+
+        public static void SetupServer(int port)
+        {
+            Console.WriteLine("Setting up server on port " + port + " ...");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+            _serverSocket.Listen(1);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+        }
+
+        private static void AcceptCallBack(IAsyncResult result)
+        {
+            Socket client = _serverSocket.EndAccept(result);
+            _clientSockets.Add(client);
+            Console.WriteLine("Client n°" + _clientSockets.Count + " is Connected.");
+            client.BeginReceive(_buffer,
+                                0,
+                                _buffer.Length,
+                                SocketFlags.None,
+                                new AsyncCallback(ReceiveCallback),
+                                client);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult result)
+        {
+            Socket client = (Socket)result.AsyncState;
+            int receive = client.EndReceive(result);
+            byte[] dataBuf = new byte[receive];
+            Array.Copy(_buffer, dataBuf, receive);
+
+            ProcessDataFromReceive(client, dataBuf);
+        }
+
+        private static void ProcessDataFromReceive(Socket client, byte[] dataBuf)
+        {
+            string debugText = Encoding.ASCII.GetString(dataBuf);
+            Console.WriteLine("Request receive : " + debugText);
+
+            string request = string.Empty;
+            if(debugText.ToLower() != "get time")
+            {
+                request = "Invalid request";
+            }
+            else
+            {
+                request = DateTime.Now.ToLongTimeString();
+            }
+            byte[] data = Encoding.ASCII.GetBytes(request);
+            SendBuffer(client, data);
+
+            client.BeginReceive(_buffer,
+                                0,
+                                _buffer.Length,
+                                SocketFlags.None,
+                                new AsyncCallback(ReceiveCallback),
+                                client);
+        }
+
+        private static void SendBuffer(Socket client, byte[] data)
+        {
+            client.BeginSend(data,
+                             0, data.Length,
+                             SocketFlags.None,
+                             new AsyncCallback(SendCallback),
+                             client);
+
+        }
+
+        private static void SendCallback(IAsyncResult result)
+        {
+            Socket client = (Socket)result.AsyncState;
+            client.EndSend(result);
+        }
+    }
+}
---
+++ LobbyServer/TesterLobby/TesterLobby/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesterLobby
+{
+    class Program
+    {
+        private static Socket _clientSocket = new Socket(
+                                                  AddressFamily.InterNetwork,
+                                                  SocketType.Stream,
+                                                  ProtocolType.Tcp);
+        private static byte[] _buffer = new byte[1024];
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            LoopConnect(args[0], Convert.ToInt32(args[1]));
+
+            LoopSend();
+
+            Console.ReadLine();
+        }
+
+        private static void LoopSend()
+        {
+            while(true)
+            {
+                Console.Write("Enter request : ");
+                string req = Console.ReadLine();
+                byte[] dataBuf = Encoding.ASCII.GetBytes(req);
+                _clientSocket.Send(dataBuf);
+
+                byte[] receivedBuf = new byte[1024];
+                int dataSize = _clientSocket.Receive(receivedBuf);
+                byte[] data = new byte[dataSize];
+                Array.Copy(receivedBuf, data, dataSize);
+                Console.WriteLine("Received : " + Encoding.ASCII.GetString(data));
+            }
+        }
+
+        private static void LoopConnect(string servAdress, int servPort)
+        {
+            int attempts = 0;
+            while(!_clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    _clientSocket.Connect(new IPEndPoint(
+                                         (Dns.GetHostAddresses(servAdress))[0],
+                                         servPort));
+                }
+                catch(SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Connection attempts : " + 
+                                      attempts +
+                                      " | IP : " +
+                                      servAdress + 
+                                      " | Port : " + 
+                                      servPort + 
+                                      " |");
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected after " + attempts + " attempts.");
+        }
+    }
+}
